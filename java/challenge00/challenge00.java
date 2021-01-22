6caa59f7efe6dcc17b13baeed2fb4294fcdc35fb
alaamimi <aku-no-hana@outlook.fr>
2021-01-22 03:16:33 +0100

    first java prog

---
+++ java/challenge00/challenge00.java
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   challenge00.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alaamimi <alaamimi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/22 03:14:39 by alaamimi          #+#    #+#             */
+/*   Updated: 2021/01/22 03:14:43 by alaamimi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+public class challenge00
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello, World.");
+		System.out.println("Hello, Java.");
+	}
+}

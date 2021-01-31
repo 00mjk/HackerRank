29ba5ba515b5799a35e036483944f634a29fb330
alaamimi <aku-no-hana@outlook.fr>
2021-01-31 11:30:51 +0100

    MOAR JAVA

--- java/challenge02/challenge02.java
+++ java/challenge02/challenge02.java
@@ -12,20 +12,18 @@
 
 import java.util.Scanner;
 
-public class Solution {
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int i = scan.nextInt();
-        double d = scan.nextDouble();
-        String nl = scan.nextLine();
-        String s = scan.nextLine();
-
-        // Write your code here.
-
-        System.out.println("String: " + s);
-        System.out.println("Double: " + d);
-        System.out.println("Int: " + i);
-    }
+public class Solution 
+{
+	public static void main(String[] args) i
+	{
+		Scanner scan = new Scanner(System.in);
+		int i = scan.nextInt();
+		double d = scan.nextDouble();
+		String nl = scan.nextLine();
+		String s = scan.nextLine();
+		System.out.println("String: " + s);
+		System.out.println("Double: " + d);
+		System.out.println("Int: " + i);
+	}
 }
 

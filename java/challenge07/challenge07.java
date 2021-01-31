83189a7f5a00848c5eca14a198b3a49d7a2bfde9
alaamimi <aku-no-hana@outlook.fr>
2021-01-31 11:17:22 +0100

    Java eof

---
+++ java/challenge07/challenge07.java
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   challenge07.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alaamimi <alaamimi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/31 11:14:36 by alaamimi          #+#    #+#             */
+/*   Updated: 2021/01/31 11:14:43 by alaamimi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int i;
+        i = 0;
+        while(scan.hasNext())
+        {
+            i++;
+            System.out.println(i + " " + scan.nextLine());
+        }
+    }
+}
